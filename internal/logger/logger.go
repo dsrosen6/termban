9f2e93442284a8ae4bf572af67b7f1feac45f0d1
tctgdanny <154454046+tctgdanny@users.noreply.github.com>
2024-10-23 21:48:12 -0500

    logger location changes

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,12 +4,30 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"os/user"
+)
+
+const (
+	logSubDir   string = "/Library/termban/Logs/"
+	logFileName string = "termban.log"
 )
 
 var log *slog.Logger
 
 func init() {
-	logFile, err := os.OpenFile("termban.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	usr, err := getCurrentUser()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	ld, err := makeLogDir(usr)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	f := ld + logFileName
+	logFile, err := os.OpenFile(f, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic(fmt.Sprintf("error opening log file: %v", err))
 	}
@@ -21,4 +39,25 @@ func init() {
 
 func GetLogger() *slog.Logger {
 	return log
+}
+
+// Create termban log directory if it doesn't exist - also return path as string for later use
+func makeLogDir(usr *user.User) (string, error) {
+	h := usr.HomeDir
+	ld := h + logSubDir
+	if err := os.MkdirAll(ld, 0755); err != nil {
+		return "", fmt.Errorf("could not create termban log folder: %w", err)
+	}
+
+	return ld, nil
+}
+
+// Get current user in order to construct log folder path
+func getCurrentUser() (*user.User, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return nil, fmt.Errorf("could not get current logged in user: %w", err)
+	}
+
+	return usr, nil
 }
--- makefile
+++ makefile
@@ -1,5 +1,8 @@
 run:
 	go run cmd/main/main.go
 
+build:
+	go build -o cmd/main/termban cmd/main/main.go
+
 log:
 	tail -f termban.log
